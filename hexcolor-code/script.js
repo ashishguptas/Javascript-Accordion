f755394d67ad20aa0eed86f999a946719aa91815
Asg21 <ashishguptam1154@gmail.com>
2020-05-01 15:12:16 +0530

    initial commit

---
+++ hexcolor-code/script.js
@@ -0,0 +1,19 @@
+const hexNum = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexbtn');
+const bodyBg = document.querySelector('body');
+const hexCode = document.querySelector('.hexcode');
+
+hexBtn.addEventListener('click', changeBg);
+ 
+function changeBg(){
+    let hexcol = '#';
+    for(i = 0; i < 6;i++){
+        const random = Math.floor(Math.random()*hexNum.length);
+        // console.log(random)
+        hexcol += hexNum[random];
+        console.log(hexcol);
+    }
+    bodyBg.style.backgroundColor = hexcol;
+    hexCode.innerHTML = hexcol;
+}
+changeBg()
